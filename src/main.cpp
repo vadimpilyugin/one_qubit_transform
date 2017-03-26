e58d6a63ce76d9557adb7539c3219ccb64b19569
vadim <vadimpilyugin@gmail.com>
2017-03-27 00:29:14 +0300

    transform works

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ typedef unsigned long int ulong;
 #define TRUE 1
 #define FALSE 0
 #define NO_MEMORY -1
+#define WRONG_VALUE -2
 
 #define MASTER 0
 #define NO_TAG 0
@@ -37,7 +38,7 @@ void stop()
 
 int generate_state(complexd **_state, const size_t number_of_qubits)
 {
-	size_t i;
+	ulong i;
 	// Vector with a quantum state of our qubits
 	complexd *state = NULL;
 	// Size of the vector, which is 2 to the power of number of qubits
@@ -77,26 +78,7 @@ int generate_state(complexd **_state, const size_t number_of_qubits)
 	const int scale = 100;
 	for(i = 0; i < portion_size; i++)
 		portion[i] = complexd(rand() / (RAND_MAX + 0.0) * scale - scale/2, rand() / (RAND_MAX + 0.0) * scale - scale/2);
-	// MPI_Barrier(MPI_COMM_WORLD);
-	// for(i = 0; i < portion_size; i++)
-	// 	printf("%d:\t( %lf, %lf )\n", myrank*portion_size + i, portion[i].real(), portion[i].imag());
-	MPI_Barrier(MPI_COMM_WORLD);
-	if(i_am_the_master) {
-		complexd zero(0,0);
-		for(i = 0; i < size; i++) {
-			state[i] = zero;
-			printf("%zu:\t( %lf, %lf )\n", i, state[i].real(), state[i].imag());
-		}
-		return SUCCESS;
-	}
-	else
-		for(i = 0; i < portion_size; i++)
-			printf("%d:\t( %lf, %lf )\n", myrank*portion_size + i, portion[i].real(), portion[i].imag());
-		
-	// MPI_Barrier(MPI_COMM_WORLD);
-	
 	MPI_Gather(portion, portion_size, MPI_DOUBLE_COMPLEX, state, portion_size, MPI_DOUBLE_COMPLEX, MASTER, MPI_COMM_WORLD);
-	printf("----------- Success!! -------------------- \n");
 	*_state = state;
 	delete [] portion;
 	return SUCCESS;
@@ -104,9 +86,23 @@ int generate_state(complexd **_state, const size_t number_of_qubits)
 
 int transform(complexd *state, const size_t number_of_qubits, const size_t qubit_num)
 {
+	#if DEBUG
+	if(i_am_the_master)
+		printf("Transforming state vector with number_of_qubits = %zu and qubit_num = %zu\n", number_of_qubits, qubit_num);
+	#endif
+	if(number_of_qubits == 0 || qubit_num == 0 || qubit_num > number_of_qubits || (state == NULL && i_am_the_master))
+		return WRONG_VALUE;
+	// State vector size
+	ulong size = 1 << number_of_qubits;
+	if(size == 1 || size <= proc_num)
+		return WRONG_VALUE;
+	ulong i;
 	// Root process sends portions of state vector
 	complexd *portion = NULL;
-	ulong size = 1 << number_of_qubits;
+	// Qubit number divides state vector into parts
+	ulong parts_num = 1 << qubit_num;
+	ulong processes_per_part = proc_num / parts_num;
+	// Each process has a portion of state vector
 	ulong portion_size = size / proc_num;
 	try
 	{
@@ -121,10 +117,8 @@ int transform(complexd *state, const size_t number_of_qubits, const size_t qubit
 	MPI_Scatter(state,portion_size,MPI_DOUBLE_COMPLEX,portion,portion_size,MPI_DOUBLE_COMPLEX,MASTER,MPI_COMM_WORLD);
 
 	// Each process has its own part of state vector
-	ulong parts_num = 1 << qubit_num;
-	ulong processes_per_part = proc_num / parts_num;
-
-	if(processes_per_part >= 1)
+	const double root2 = sqrt(2);
+	if(processes_per_part >= 1 && portion_size > 1)
 	{
 		int i_am_white = (myrank & processes_per_part) == processes_per_part;
 		// We need an extra Sendrecv operation
@@ -135,44 +129,29 @@ int transform(complexd *state, const size_t number_of_qubits, const size_t qubit
 		else
 			sendrecv_buffer = portion + portion_size / 2;
 		MPI_Status temp;
+		MPI_Sendrecv_replace(sendrecv_buffer, portion_size/2, MPI_DOUBLE_COMPLEX, myrank^processes_per_part, NO_TAG, myrank, NO_TAG, MPI_COMM_WORLD, &temp);
+		// Transform first half of the vector with the second half
+		for(i = 0; i < portion_size/2; i++) {
+			portion[i] = (portion[i] + portion[i+portion_size/2]) / root2;
+			portion[i+portion_size/2] = (portion[i] - portion[i+portion_size/2]) / root2;
+		}
+		// We need an extra Sendrecv operation to restore order
 		MPI_Sendrecv_replace(sendrecv_buffer, portion_size/2, MPI_DOUBLE_COMPLEX, myrank^processes_per_part, NO_TAG, myrank, NO_TAG, MPI_COMM_WORLD, &temp);
 	}
-	// const ulong start_pos = myrank * portion_size;
-	const ulong mask = 1 << (number_of_qubits - qubit_num + 1);
-	const double root2 = sqrt(2);
-	ulong i;
-	for(i = 0; i < portion_size; i++)
-		if((i & mask) != mask)
-		{
-			ulong index1;
-			ulong index2;
-			
-			index1 = i;
-			if(processes_per_part >= 1)
-				index2 = i + portion_size / 2;
-			else
-				index2 = i|mask;
-
-			// printf("%d and %d\n",index1,index2);
-			complexd value1 = portion[index1];
-			complexd value2 = portion[index2];
-			complexd sum = (value1 + value2)/root2;
-			complexd diff = (value1 - value2)/root2;
-			portion[index1] = sum;
-			portion[index2] = diff;
-		}
-	if(processes_per_part >= 1)
+	else if(processes_per_part == 0)
 	{
-		int i_am_white = (myrank & processes_per_part) == processes_per_part;
-		// We need an extra Sendrecv operation
-		complexd *sendrecv_buffer = NULL;
-		// half of the data will be sent
-		if(i_am_white)
-			sendrecv_buffer = portion;
-		else
-			sendrecv_buffer = portion + portion_size / 2;
-		MPI_Status temp;
-		MPI_Sendrecv_replace(sendrecv_buffer, portion_size/2, MPI_DOUBLE_COMPLEX, myrank^processes_per_part, NO_TAG, myrank, NO_TAG, MPI_COMM_WORLD, &temp);
+		// Each process has one or more blue-white pairs, so we don't need extra send operations
+		const ulong mask = 1 << (number_of_qubits - qubit_num + 1);
+		for(i = 0; i < portion_size; i++)
+			if((i & mask) != mask)
+			{
+				ulong index1 = i;
+				ulong index2 = i|mask;
+				complexd value1 = portion[index1];
+				complexd value2 = portion[index2];
+				portion[index1] = (value1 + value2)/root2;;
+				portion[index2] = (value1 - value2)/root2;;
+			}
 	}
 	MPI_Gather(portion,portion_size,MPI_DOUBLE_COMPLEX,state,portion_size,MPI_DOUBLE_COMPLEX,MASTER,MPI_COMM_WORLD);
 	delete [] portion;
@@ -217,32 +196,58 @@ int equal(complexd *state, complexd *state_copy, size_t number_of_qubits)
 	return TRUE;
 }
 
+int pv(complexd *state, size_t number_of_qubits)
+{
+	ulong size = 1 << number_of_qubits;
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		printf("%d:  ( %lf, %lf )\n", i, state[i].real(), state[i].imag());
+	}
+	return SUCCESS;
+}
+
 int test(const size_t number_of_qubits, const size_t qubit_num)
 {
 	complexd *state = NULL, *state_copy = NULL;
 	int code = generate_state(&state, number_of_qubits);
 	if(code != SUCCESS)
 		MPI_Abort(MPI_COMM_WORLD, code);
-	else if(i_am_the_master)
+	srand(72328 + myrank);
+	code = generate_state(&state_copy, number_of_qubits);
+	if(code != SUCCESS)
+		MPI_Abort(MPI_COMM_WORLD, code);
+	if(i_am_the_master)
+	{
+		assert(state != NULL);
+		assert(state_copy != NULL);
+		printf("Checking if copy of the state is equal to the original......");
+		// printf("original vector:\n");
+		// pv(state,number_of_qubits);
+		// printf("copy:\n");
+		// pv(state_copy, number_of_qubits);
+		code = equal(state,state_copy,number_of_qubits);
+		printf("%s\n", code ? "True" : "False");
+		assert(code == TRUE);
+	}
+	code = transform(state, number_of_qubits, qubit_num);
+	if(code != SUCCESS)
+		MPI_Abort(MPI_COMM_WORLD, code);
+	code = transform(state, number_of_qubits, qubit_num);
+	if(code != SUCCESS)
+		MPI_Abort(MPI_COMM_WORLD, code);
+	if(i_am_the_master) {
+		if(code = equal(state,state_copy,number_of_qubits))
+			printf("States are equal? True\n");
+		else
+			printf("States are equal? False\n");
+	}
+	if(i_am_the_master)
+	{
 		delete [] state;
-	// code = generate_state(&state_copy, number_of_qubits, 72325);
-	// if(code != SUCCESS)
-		// MPI_Abort(MPI_COMM_WORLD, code);
-	// printf("Checking if copy of the state is equal to the original...");
-	// code = equal(state,state_copy,number_of_qubits);
-	// printf("%d\n", code);
-	// assert(code == TRUE);
-	// code = transform(state, number_of_qubits, qubit_num);
-	// if(code != SUCCESS)
-	// 	MPI_Abort(MPI_COMM_WORLD, code);
-	// code = transform(state, number_of_qubits, qubit_num);
-	// if(code != SUCCESS)
-	// 	MPI_Abort(MPI_COMM_WORLD, code);
-	// if(code = equal(state,state_copy,number_of_qubits))
-	// 	printf("States are equal? True\n");
-	// else
-	// 	printf("States are equal? False\n");
-	return code;
+		delete [] state_copy;
+	}
+	return SUCCESS;
 }
 
 int main(int argc, char **argv)
@@ -253,45 +258,15 @@ int main(int argc, char **argv)
     MPI_Comm_size (MPI_COMM_WORLD, &proc_num);
 
 	i_am_the_master = myrank == MASTER;
-	
-	// int size = 8;
-	// double *state = NULL;
-	// if(i_am_the_master)
-	// 	state = new double[size];
-	// int i = 0;
-	// // Number of processes is 8
-	// assert(size == proc_num);
-	// double buffer = myrank;
-	// MPI_Gather(&buffer, 1, MPI_DOUBLE, state, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	// if(i_am_the_master)
-	// 	for(i = 0; i < size; i++)
-	// 		printf("%d:\t%lf\n", i, state[i]);
-	// if(i_am_the_master)
-	// 	delete [] state;
 
-	int size = 8;
-	complexd *state = NULL;
-	if(i_am_the_master)
-		state = new complexd[size];
-	int i = 0;
-	// Number of processes is 8
-	assert(size == proc_num);
-	complexd buffer = complexd(myrank, 0);
-	MPI_Gather(&buffer, 1, MPI_DOUBLE_COMPLEX, state, 1, MPI_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
-	if(i_am_the_master)
-		for(i = 0; i < size; i++)
-			printf("%d:\t%lf\n", i, state[i].real());
-	if(i_am_the_master)
-		delete [] state;
+	srand(72328 + myrank);
 
-	// srand(72328 + myrank);
-
-	// assert(MPI_DATATYPE_NULL != MPI_DOUBLE_COMPLEX);
+	assert(MPI_DATATYPE_NULL != MPI_DOUBLE_COMPLEX);
 	
 
 
 	// Qubits number are 20,24,25,26
-	// test(4, 2);
+	test(24,5);
 //	median(24);
 //	median(25);
 //	median(26);
